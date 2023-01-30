2800689cdf5de59d99dac45e72a81137c4f59f6f
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-01-30 23:22:54 +0200

    task-07-02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,17 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryList = document.querySelector(".gallery");
-// console.log(galleryList);
+const addListGallery = itemGallery(galleryItems);
 
-const itemGallery = ({ preview, original, description }) => {
-  return `<div class="gallery__item">
+galleryList.insertAdjacentHTML("afterbegin", addListGallery);
+
+galleryList.addEventListener("click", onOpenImg);
+galleryList.addEventListener("keydown", onCloseImg);
+
+function itemGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
       <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
@@ -15,13 +22,9 @@ const itemGallery = ({ preview, original, description }) => {
         />
       </a>
     </div>`;
-};
-
-const addListGallery = galleryItems.map(itemGallery).join("");
-galleryList.insertAdjacentHTML("afterbegin", addListGallery);
-
-galleryList.addEventListener("click", onOpenImg);
-galleryList.addEventListener("keydown", onCloseImg);
+    })
+    .join("");
+}
 
 function onOpenImg(event) {
   event.preventDefault();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listGallery = document.querySelector(".gallery");
+const pictureMarkup = onCreatePictureMarkup(galleryItems);
+
+listGallery.insertAdjacentHTML("afterbegin", pictureMarkup);
+
+listGallery.addEventListener("click", onOpenModal);
+
+function onCreatePictureMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>`;
+    })
+    .join("");
+}
+
+function onOpenModal(event) {
+  event.preventDefault();
+
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionPosition: "bottom",
+  });
+}
